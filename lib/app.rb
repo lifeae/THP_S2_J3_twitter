172fc5264b2f37b47bfebf8299fb4a29d79e1c23
lifeae <romain.leporatti@laposte.net>
2020-01-22 10:29:17 +0100

    first example of app_spec.rb work

--- lib/app.rb
+++ lib/app.rb
@@ -5,12 +5,19 @@ Dotenv.load('.env') # Ceci appelle le fichier .env (situé dans le même dossier
 # et grâce à la gem Dotenv, on importe toutes les données enregistrées dans un hash ENV
 
 # quelques lignes qui appellent les clés d'API de ton fichier .env
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+def login_twitter
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+  end
+  return client
 end
 
 # ligne qui permet de tweeter sur ton compte
-client.update('Mon premier tweet en Ruby ! - Lifeae')
+def tweet(your_tweet)
+  puts "Que veux-tu tweeter ?"
+  your_tweet = gets.chomp
+  client.update(your_tweet)
+end
